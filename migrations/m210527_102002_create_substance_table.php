250f36a426927a5caef4d61acdcfa871b92de8c5
restlin <restlin@w-105>
2021-05-28 22:57:03 +0400

    [add] добавлены миграции user_diagnosis и diagnosis_substance

--- migrations/m210527_102002_create_substance_table.php
+++ migrations/m210527_102002_create_substance_table.php
@@ -17,6 +17,32 @@ class m210527_102002_create_substance_table extends Migration
             'name' => $this->string(100)->notNull()->comment('Наименование'),
             'description' => $this->text()->notNull()->comment('Описание'),
         ]);
+
+        $this->createTable('{{%diagnosis_substance}}', [
+            'id' => $this->primaryKey(),
+            'substance_id' => $this->integer()->notNull()->comment('Активное вещество'),
+            'diagnosis_id' => $this->integer()->notNull()->comment('Диагноз'),
+        ]);
+
+        $tableName = "diagnosis_substance";
+        $this->addForeignKey("fk_{$tableName}_diagnosis_id", $tableName, 'diagnosis_id', 'user', 'id', 'CASCADE', 'CASCADE');
+        $this->createIndex("idx_{$tableName}_diagnosis_id", $tableName, 'diagnosis_id');
+        $this->addForeignKey("fk_{$tableName}_substance_id", $tableName, 'substance_id', 'substance', 'id', 'CASCADE', 'CASCADE');
+        $this->createIndex("idx_{$tableName}_substance_id", $tableName, 'substance_id');
+
+
+        $this->createTable('{{%user_diagnosis}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull()->comment('Пользователь'),
+            'diagnosis_id' => $this->integer()->notNull()->comment('Диагноз'),
+            'regular' => $this->boolean()->notNull()->defaultValue(false)->comment('Регулярный прием'),
+        ]);
+
+        $tableName = "user_diagnosis";
+        $this->addForeignKey("fk_{$tableName}_diagnosis_id", $tableName, 'diagnosis_id', 'user', 'id', 'CASCADE', 'CASCADE');
+        $this->createIndex("idx_{$tableName}_diagnosis_id", $tableName, 'diagnosis_id');
+        $this->addForeignKey("fk_{$tableName}_user_id", $tableName, 'user_id', 'user', 'id', 'CASCADE', 'CASCADE');
+        $this->createIndex("idx_{$tableName}_user_id", $tableName, 'user_id');
     }
 
     /**
@@ -24,6 +50,8 @@ class m210527_102002_create_substance_table extends Migration
      */
     public function safeDown()
     {
+        $this->dropTable('{{%user_diagnosis}}');
+        $this->dropTable('{{%diagnosis_substance}}');
         $this->dropTable('{{%substance}}');
     }
 }
